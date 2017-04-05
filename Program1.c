d3a172a7ab5c91d180e6c52d9a18a312c9aeda1b
sanderau <sanderau@oregonstate.edu>
2017-04-05 14:26:21 -0700

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,35 @@
+/* CS261- HW1 - Program2.c*/
+/* Name:
+ * Date:
+ * Solution description:
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int* a, int* b, int c){
+    /* Increment a */
+    
+    /* Decrement  b */
+    
+    /* Assign a-b to c */
+    
+    /* Return c */
+}
+
+int main(){
+    /* Declare three integers x,y and z and initialize them randomly to values in [0,10] */
+    
+    /* Print the values of x, y and z */
+    
+    /* Call foo() appropriately, passing x,y,z as parameters */
+    
+    /* Print the values of x, y and z */
+    
+    /* Print the value returned by foo */
+ 
+    /* Is the return value different than the value of z?  Why? */
+    return 0;
+}
+    
+    
---
+++ Program4.c
@@ -0,0 +1,33 @@
+/* CS261- HW1 - Program5.c*/
+/* Name:
+ * Date:
+ * Solution description:
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student{
+	char initials[2];
+	int score;
+};
+
+void sort(struct student* students, int n){
+     /*Sort the n students based on their initials*/     
+}
+
+int main(){
+    /*Declare an integer n and assign it a value.*/
+    
+    /*Allocate memory for n students using malloc.*/
+    
+    /*Generate random IDs and scores for the n students, using rand().*/
+    
+    /*Print the contents of the array of n students.*/
+
+    /*Pass this array along with n to the sort() function*/
+    
+    /*Print the contents of the array of n students.*/
+    
+    return 0;
+}
